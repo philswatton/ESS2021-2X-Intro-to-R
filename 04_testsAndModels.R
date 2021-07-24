606d48dfe6b4b26a99d575ae8b9ecb48e8c2ea15
Phil Swatton <philswatton@gmail.com>
2021-07-24 12:00:48 +0100

    linear models

--- 04_testsAndModels.R
+++ 04_testsAndModels.R
@@ -18,13 +18,15 @@ library(texreg)
 
 
 ## Data
+aoe <- read.csv("data/match_players.csv") %>%
+  mutate(win = as.logical(tolower(winner)))
 avocado <- read.csv("data/avocado.csv")
-simpsons <- read.csv("data/simpsons_episodes.csv")
+books <- read.csv("data/bestsellers with categories.csv")
 
 
 
 
-## 1 T-tests ----
+# 1 t-tests ----
 
 # As you may know, the reason why millennials can't afford a home is because
 # they buy too much avocado toast. We're going to perform a t-test to get a
@@ -33,6 +35,9 @@ simpsons <- read.csv("data/simpsons_episodes.csv")
 # To test whether the mean avocado price is different from 0, we can use
 # the t.test function:
 t.test(avocado$AveragePrice) # by default a two-tailed t-test with null value = 0
+
+# the t.test() function is VERY customisable to your needs:
+?t.test
 
 # We can also test to see whether the avocado price is the same as other values,
 # like the median price of a house in the US:
@@ -46,7 +51,6 @@ t.test(avocado$AveragePrice, mu = 269039, alternative = "greater")
 # to test to see whether conventional and organic avocados have the same mean
 # price, so we can advise millennials looking to buy a house on whether avoiding
 # organic will help them save up.
-
 t.test(x = avocado$AveragePrice[avocado$type == "conventional"],
        y = avocado$AveragePrice[avocado$type == "organic"])
 
@@ -54,96 +58,163 @@ t.test(x = avocado$AveragePrice[avocado$type == "conventional"],
 # home ownership...
 
 
+# There are lots of other tests in base R via the stats package. These include
+# but are by no means limited to:
+# chisq.test() # Chi Square test
+# prop.test() # Proportion test
+# wilcox.test() # both Wilcoxon and Mann-Whitney U test
+
+# As always, if you need to find a certain test - google is your friend!
+
+
 
 
 # 2 Statistical models ----
 
-# We're now going to switch things up and look at a dataset of 
+## 2.1 OLS ----
+
+# We're now going to switch things up and look at a dataset of Amazon bestselling
+# books. We're interested in the effect of price and genre (fiction or
+# non-fiction) on reader reviews.
+
+# Let's start with a simple model looking at price. We build models in R like
+# so:
+
+model1 <- User.Rating ~ Price
+
+# Our DV goes on the left-hand side of the tilde (~), our independent variables
+# on the right-hand side. Here, we are regressing User.Rating on Price (note
+# that these need to match the variable names exactly)
+
+# We can then use the lm() function to run our model:
+result1 <- lm(model1, books)
 
 
-# suppose now we want to estimate a linear model using OLS. 
-# To do that we first import a dataset from an article by David Card (1993) on NBER:
-card <- read.dta("Card_data.dta")
-head(card)
-
-library(haven)
-card <- read_dta("Card_data.dta")
+# Base R offers a way of looking at our results with the summary() function:
+summary(result1)
 
 
-# We might check it with a linear model:
-model1 <- lm(wage ~ exper, # your formula
-             data = card) # where are the data stored
-summary(model1)
+# But we probably want to look at more than one variable - let's also look
+# at genre. We add more variables with a + sign followed by the next variable
+# name:
 
-# we probably would want to include some control variables to this model:
-model2 <- lm(wage ~ exper + motheduc + fatheduc,
-             data = card)
-summary(model2)
+model2 <- User.Rating ~ Price + Genre
+result2 <- lm(model2, books)
+summary(result2)
 
-# notice we can transform variables. For instance, we can use the log of wage as a dv:
-model3 <- lm(log(wage) ~ exper + motheduc + fatheduc,
-             data = card)
-summary(model3)
+# Notice that the character variable Genre was automatically treated as 
+# a factor variable. Obviously, factors are too!
 
-# maybe then we want to show our results. There are some very good packages to produce
-# neat regression tables out there. The choice between them depends mostly on the type
-# of models that they can support. Two equally good choices are stargazer and texreg.
-# We'll use stargazer today:
+# For those who use STATA, this differs a lot from what you're used to - declaring
+# variables as factors in the model. In R, the data type of your variables mattrs
+# much more.
 
 
-# let's see how stargazer works
-stargazer(model1, model2, model3, type = "text",
-          keep.stat = c("n", "rsq", "adj.rsq", "f"), df = FALSE)
-# maybe we want to export it to a Word document:
-stargazer(model1, model2, model3, type = "html",
-          keep.stat = c("n", "rsq", "adj.rsq", "f"), df = FALSE,
-          out = "my_table.doc")
-# or as a txt document:
-stargazer(model1, model2, model3, type = "text",
-          keep.stat = c("n", "rsq", "adj.rsq", "f"), df = FALSE,
-          out = "my_table.txt")
-# or maybe as a latex document:
-stargazer(model1, model2, model3, type = "latex",
-          keep.stat = c("n", "rsq", "adj.rsq", "f"), df = FALSE,
-          out = "my_table.tex")
-?stargazer
 
-# we can also extract statistics and information from our model objects:
-model3$coefficients
-model3$residuals
-fit <- model3$fitted.values
 
-# and more:
-summary(model3)$coefficients[,1] # these are the coefficients (the betas)
-summary(model3)$coefficients[,2] # these are the standard errors of our coefficients
-summary(model3)$coefficients[,3] # these are the t-statistics
-summary(model3)$coefficients[,4] # these are the p-values
-# of course we could save this information in R objects
+## 2.2 Outputting results ----
 
-# if we want to obtain predictions from our model, together with confidence intervals,
-# the predict() function is our friend:
+# Since you'll probably want to produce nice tables of your regression results,
+# I've included two good packages (stargazer and texreg) for doing this in R. 
+# Both are compatible with a very wide range of R packages.
+
+# I personally prefer texreg of the two, so we'll be using that.
+
+# Texreg contains three main workhorse functions: texreg, screenreg, htmlreg.
+
+# The first creates LaTeX output, the second is good for building tables within
+# R, the last one creates HTML output (this is what you'll want if you're writing
+# papers in word. Word can open them and you'll see them as regular tables, and
+# you'll be able to copy them over to a regular word document)
+
+# Let's get going:
+modelList <- list(result1, result2)
+screenreg(modelList)
+
+# There's a huge scope for customisation with texreg:
+?screenreg # most of the important arguments are the same for the three functions
+screenreg(modelList,
+          custom.model.names = c("Partial", "Full"),
+          custom.coef.names = c("Intercept", "Price", "Non-Fiction"),
+          custom.gof.names = c("R-Squared", "Adjusted R-Squared", "N"))
+
+# texreg() and htmlreg() generally take the same arguments in terms of 
+# customisation, with a few specific differences relating to LaTeX/HTML specific
+# things
+texreg(modelList,
+       custom.model.names = c("Partial", "Full"),
+       custom.coef.names = c("Intercept", "Price", "Non-Fiction"),
+       custom.gof.names = c("R-Squared", "Adjusted R-Squared", "N"),
+       float.pos = "h")
+
+htmlreg(modelList,
+        custom.model.names = c("Partial", "Full"),
+        custom.coef.names = c("Intercept", "Price", "Non-Fiction"),
+        custom.gof.names = c("R-Squared", "Adjusted R-Squared", "N"))
+
+
+
+
+## 2.3 Useful stuff ----
+
+# Remember lists? notice something particular about our outputs:
+is.list(result2)
+is.list(summary(result2))
+
+
+# This means if we know the structure of these objects, we can pull out
+# useful information by knowing how to subset lists!
+
+# This is a lot when you're new to R:
+str(result2)
+
+# But we can do lots of useful things:
+result2$coefficients
+result2$residuals
+result2$fitted.values
+
+# The coefficients from the summary() are stored in a matrix:
+summary(result2)$coefficients
+
+# Meaning we can 'chain' our subsetting to get lots of info out of them:
+summary(result2)$coefficients[,2] # Standard errors of the coefficients
+summary(result2)$coefficients[,4] # P-values of the coefficients
+
+# Naturally, we can put this information in R objects!
+
+# Finally
 prediction <- as.data.frame(predict(model1, interval = "confidence"))
 ?predict
 
-# those of you who needed to obtain robust or clustered standard errors will definitely
-# need the package sandwich, which contains functions like vcovHC() and vcovCL() that compute
-# variance-covariance matrixes to obtain standard errors that are robust 
-# to heteroskedasticity or clustered at some data level. But enough on that for today. 
 
-### 4.2.2 generalized linear models ----
 
-# Most statistical models in R have a syntax very similar to the one adopted by lm().
-# Covering these models is not part of today's class, but we might want to show
-# that other models, such as those in the family of the generalized linear model (GLM),
-# work very similarly. Suppose we want to explain if a person went to college based on the
-# years of education of the father and mother. A logit or probit model would be appropriate:
 
-mod.glm <- glm(enroll ~ fatheduc + motheduc +
-                 black + smsa + south + IQ,
-               data = card,
-               family = binomial(link = "logit"))
-summary(mod.glm)
+## 2.4 Generalized linear models ----
 
-# Time for exercises!
+# In R, the majority of models you'll use (both from base R and other packages)
+# have syntaxes similar to lm(). The formula structure is fairly ubiquitous,
+# with some adjusments depending what the package is adding.
 
-# THE END ----
+# We won't be covering these packges or models in today's class (as we risk
+# going outside of general stats knowledge/stuff that's useful for
+# everyone) but we will briefly look at generalized linear models.
+
+# If you haven't heard the term before, this includes things like logit and
+# probit models.
+
+# Let's look at the effect of player ranking and civ on likelihood to win.
+
+# We build our model in much the same way:
+glmModel <- win ~ rating + civ
+
+# And run it with the glm() function:
+logit <- glm(glmModel, aoe, family = binomial(link = "logit"))
+probit <- glm(glmModel, aoe, family = binomial(link = "probit"))
+
+# Summary() and screenreg() work just as well here too:
+screenreg(list(logit, probit))
+
+# That's all for today folks!
+
+
+
